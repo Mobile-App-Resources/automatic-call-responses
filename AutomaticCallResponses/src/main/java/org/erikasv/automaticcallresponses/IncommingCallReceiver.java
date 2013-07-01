22c40fff6550e7745414a868babae385fa67b914
Erika Suárez Valencia <marvinesv@gmail.com>
2013-07-01 18:42:27 -0500

    Comentarios y filtro para las llamadas perdidas

--- AutomaticCallResponses/src/main/java/org/erikasv/automaticcallresponses/IncommingCallReceiver.java
+++ AutomaticCallResponses/src/main/java/org/erikasv/automaticcallresponses/IncommingCallReceiver.java
@@ -10,7 +10,7 @@ import android.util.Log;
 /**
  * Created by andresrtm on 1/07/13.
  */
-public class IncommingCallReceiver  extends BroadcastReceiver {
+public class IncommingCallReceiver extends BroadcastReceiver {
     private static long timeStarted = -1L; // IMPORTANT!
     private static long timeAnswered;
     private static long timeEnded;
@@ -20,21 +20,17 @@ public class IncommingCallReceiver  extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
         Bundle extras = intent.getExtras();
         if (extras == null) return;
         String state = extras.getString(TelephonyManager.EXTRA_STATE);
         if (state == null) return;
 
-
         // phone is ringing
         if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
 
-
             timeStarted = System.currentTimeMillis();
             callerNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(
-                    Context.TELEPHONY_SERVICE);
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             isRoaming = tm.isNetworkRoaming();
 
             // set timeAnswered to -1;
@@ -45,24 +41,20 @@ public class IncommingCallReceiver  extends BroadcastReceiver {
             Log.d("TEST", "isRoaming: " + isRoaming);
 
         }
-
-        // call was answered
+        // call was answered -> Esto no nos interesa
         else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK) && timeStarted != -1L) {
-
-
             timeAnswered = System.currentTimeMillis();
             Log.d("TEST", "timeAnswered: " + timeAnswered);
-
         }
-
         // call was ended
+        /** EXISTIO UNA LLAMADA, pero no diferncia enrte si fue contestada o no*/
         else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE) && timeStarted != -1L) {
-
-
-            timeEnded = System.currentTimeMillis();
-            Log.d("TEST", "timeEnded: " + timeEnded);
-            timeStarted = -1L; // DON'T FORGET!
-
+            /**Aqui verifico que sea perdida*/
+            if (timeAnswered == -1L) {
+                timeEnded = System.currentTimeMillis();
+                Log.d("TEST", "timeEnded: " + timeEnded);
+                timeStarted = -1L; // DON'T FORGET!
+            }
         }
     }
 }
